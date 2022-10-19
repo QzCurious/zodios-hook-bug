94680ba8dc29eb7eaa06507e0e2afaca87eed692
qz <qzcurious@gmail.com>
2022-10-19 17:57:47 +0800

    react-query works fine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  useQuery(['works'], () => 'works')
+
   return (
     <div className={styles.container}>
       <Head>
